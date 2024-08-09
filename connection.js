0858b481880723ae56b1eaf91f9bc2e64d6fbda1
Darshan98Solanki <106055701+Darshan98Solanki@users.noreply.github.com>
2024-08-09 10:08:17 +0530

    n

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2")
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'shyam_dudh_dairy'
+    host: 'srv1274.hstgr.io',
+    user: 'u830888577sddMJA',
+    password: 'Anomaly@2023',
+    database: 'u830888577_sddApplication'
 })
 
 conn.connect((err)=>{
