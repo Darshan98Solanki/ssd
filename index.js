b3f8c6fd546fd266aabfdfe1687724c7abcd7297
Darshan98Solanki <106055701+Darshan98Solanki@users.noreply.github.com>
2024-08-09 18:49:14 +0530

    new change

--- index.js
+++ index.js
@@ -793,14 +793,14 @@ app.get('/get_full_report', async (req, res) => {
 
         conn.query(getUserData, [organization, userId], (err, customerData) => {
             if (err) {
-                res.status(411).json({ message: "Some error 1 occurred..." })
+                res.status(411).json({ message: "Some error occurred..." })
                 return
             } else {
                 const query = "SELECT p.fat,p.purchase_date,p.amount,p.litre FROM customers c INNER JOIN purchase p ON c.customer_id = p.customer_id AND p.payment_status != 'paid' AND c.organization = ? AND c.user_id = ?"
 
                 conn.query(query, [organization, userId], (err, result) => {
                     if (err) {
-                        res.status(411).json({ message: "Some lol occurred..." })
+                        res.status(411).json({ message: "Some error occurred..." })
                         return
                     } else {
                         if (result.length == 0) {
